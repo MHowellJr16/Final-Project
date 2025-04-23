1c3dfa13fa561d40b2f35084e1da63892cdf364d
Mark <mhowelljr16@gmail.com>
2025-04-23 07:58:22 -0400

    Apr. 23__Update

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ function toggleDarkMode() {
  *  * 2. ASYNC/AWAIT method 
  * */
 // THEN method:
-const emailRef = document.querySelector('.email');
-console.log(emailRef)
+/**
+ * const emailRef = document.querySelector(".email");
 fetch('https://jsonplaceholder.typicode.com/users').then(response => {
     /**
      *  Inside Callback, use ".json" to
@@ -73,9 +73,47 @@ fetch('https://jsonplaceholder.typicode.com/users').then(response => {
      *  Printing SINGLE ELEMENT in API
      * THis is what the EMAILREF is for:
      */
-    console.log(response.json().then(data => {
-        console.log(data)
-        emailRef.innerHTML = data.name
-    }))
-})
+   // console.log(response.json().then(data => {
+       // console.log(data)
+       // emailRef.innerHTML = data.email
+   // })
+// )
+//})
+// */
 // TIME STAMP: 8:47
+/**
+ * Short Way: 
+ * fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+ *      return response.json() {
+    }).then(data => {
+        console.log(data)    
+    })
+ */
+/**
+ * 
+ * ************CORRECT CODE:****************** 
+ * const emailRef = document.querySelector(".email")
+fetch('https://jsonplaceholder.typicode.com/users').then(response => 
+    response.json())
+    .then(data => {
+console.log(data); // data is an array
+emailRef.innerHTML = data[0].email; // using the first user's email
+})
+*/
+// ASYNC/AWAIT METHOD:
+/**
+ * Starts with an ASYNCE FUNCTION:
+ * The AWAIT (aka WAITING on something)
+ * is INSIDE the ASYNC funciton
+ */
+const emailRef = document.querySelector(".email")
+async function main() {
+    // Then must make await a VARIABLE
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data[0].email
+}
+// Then CALL function as usual:
+main()
+// TIME STAMP: 16:02
