3df36e97a951e8d76c7023d360dd13471c1c4a1a
Mark <mhowelljr16@gmail.com>
2025-04-25 14:31:34 -0400

    Apr. 25__Promises and Questions

--- app.js
+++ app.js
@@ -106,34 +106,34 @@ emailRef.innerHTML = data[0].email; // using the first user's email
  * The AWAIT (aka WAITING on something)
  * is INSIDE the ASYNC funciton
  */
-const emailRef = document.querySelector(".email")
-async function main() {
+// const emailRef = document.querySelector(".email")
+// async function main() {
     // Then must make await a VARIABLE
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await response.json()
-    console.log(data)
-    emailRef.innerHTML = data[0].email
-}
+    //const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    //const data = await response.json()
+    //console.log(data)
+    //emailRef.innerHTML = data[0].email
+// }
 // Then CALL function as usual:
-main()
+//main()
 // TIME STAMP: 16:02
 /**
  * *************CREATING A PROMISE****************
  */
 // How you DISPLAY code ON WEBPAGE
 // Have to DECLARE VARIABLE that LINKS to JS
-const statusRef = document.querySelector(".status")
-const videoREf = document.querySelector(".video")
-function getSubsrciptionStatus() {
+//const statusRef = document.querySelector(".status")
+//const videoRef = document.querySelector(".video")
+// function getSubsrciptionStatus() {
     /**Inside Promise callback,
      * write 'RESOLVE' and 'REJECT'
      * Resolve is for if something is SUCCESSFUL
      * Reject is for NONAPPLICABLE(I.E. EVERYTHING ELSE)
      */
-    return new Promise((resolve, reject) => {
-        resolve("VIP")
-    })
-}
+  //  return new Promise((resolve, reject) => {
+    //    resolve("VIP")
+  //  })
+//}
 // You then use Async/Await or Then to UNLOCK PROMISE:
 // 1. THEN METOD:
 // getSubsrciptionStatus().then(response => console.log(response))
@@ -147,7 +147,7 @@ async function mainFunction(params) {
 mainFunction()
 */
 // Video Challenge
-function getVideo(subscriptionStatus) {
+/** function getVideo(subscriptionStatus) {
     return new Promise((resolve, reject) => {
         if (subscriptionStatus === "VIP") {
             resolve("Show Video")
@@ -163,14 +163,118 @@ function getVideo(subscriptionStatus) {
 
 async function mainFunction(params) {
     const status = await getSubsrciptionStatus()
-    status.innerHTML = status
+    statusRef.innerHTML = status
     try {
         console.log(await getVideo(status))
     }
     catch(e) {
         console.log(e)
-        videoREf.innerHTML = e
+        videoRef.innerHTML = e
     }
 }
 mainFunction()
+*/
 // TIME STAMP: 27:32
+/**
+ * This is the CORRECT, ALTERED CODE
+ * given by Victor Alves of FES:
+ */
+document.querySelector("#title");
+
+console.log(document.querySelector("#title"));
+
+document.getElementById("title");
+console.log(document.getElementById("title"));
+
+const emailRef = document.querySelector(".email");
+async function main() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+  emailRef.innerHTML = data[0].email;
+}
+
+main();
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video");
+function getSubsrciptionStatus() {
+  return new Promise((resolve, reject) => {
+    resolve("VIP");
+  });
+}
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("Show Video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("Show Trailer");
+    } else {
+      reject("no Video");
+    }
+  });
+}
+
+async function mainFunction() {
+    const status = await getSubsrciptionStatus();
+    statusRef.innerHTML = status;  // Use statusRef instead of status
+    
+    try {
+      const video = await getVideo(status);
+      videoRef.innerHTML = video;  // Add this line to set the video content
+    } catch (e) {
+      console.log(e);
+      videoRef.innerHTML = e;
+    }
+  }
+mainFunction();
+/**
+ * **********PROBLEM SOLVING**************
+ */
+// Problem 1: Adding two numbers together
+function addtion(num1, num2) {
+    return num1 + num2
+}
+console.log(addtion(3,2))
+// Problem 10: calculate remainder of two numbers
+function calcRemainder(num1, num2) {
+    return num1 % num2
+}
+console.log(calcRemainder(5, 2))
+// MEDIUM CHALLENGES:
+// Problem 1: Return Falsy Value
+ function filterOutFalsy(elem1, elem2) {
+    if (!elem1) {
+        return elem1
+    }
+    else {
+        return elem2
+    } 
+}
+// Using TERNARY OPERATORS:
+function newFilterOutFalsy(elem1, elem2) {
+    return !elem1 ? elem1 : elem2
+}
+console.log(filterOutFalsy(true, "dog"))
+console.log(newFilterOutFalsy(true, "dog"))
+// Problem 2: Return the length of anny array
+function arrLength(arr) {
+    return arr.length 
+}
+console.log(arrLength([1, 2, 3, 6]))
+// ADVANCED CHALLENGES:
+/**
+ * Problem 1: Show Rating: 
+ * Star(*) for Full Rating 
+ * Period(.) for Half Rating
+ */
+function showRating(rating) {
+    let ratings = ""
+    for (let i = 0; i < rating; i++) {
+        ratings = ratings + "*"
+    }
+    if (!Number.isInteger(rating)) {
+       ratings = ratings + "."
+    }
+    return ratings
+}
+console.log(showRating(3.5))
